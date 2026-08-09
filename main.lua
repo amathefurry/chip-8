987b1e4498a718cb2037544739388156a57e5a8c
ama <sveps29@gmail.com>
2026-08-09 17:02:10 +0300

    it doesnt run into errors but gives me a black screen

--- main.lua
+++ main.lua
@@ -2,8 +2,9 @@
 local MEMORY_SIZE = 0x1000 -- 4096 bits
 local PROGRAM_START = 0x200 -- first 512 bits are reserved for the interpreter
 local MEMORY = {}
+local data = {}
 
-function meminit()
+function mem_init()
     for i = 0, 0xFFF do
         MEMORY[i] = 0
     end
@@ -69,6 +70,7 @@ local function reset() -- resets everything
         for x = 1, DISPLAY_WIDTH do -- number of cols, width
             DISPLAY[y][x]=false
         end
+    end
 
         -- TODO: clear stack
     
@@ -88,7 +90,7 @@ local function reset() -- resets everything
     -- TODO: deal with timers and sp
 end
 
-local function clear_screen() -- resets screen (literally copied some of the earlierr reset() function)
+local function clear_diplay() -- resets screen (literally copied some of the earlierr reset() function)
      for y = 1, DISPLAY_HEIGHT do -- number of rows, height (yes, these comments were copied too)
             DISPLAY[y] = {}
         for x = 1, DISPLAY_WIDTH do -- number of cols, width
@@ -96,6 +98,11 @@ local function clear_screen() -- resets screen (literally copied some of the ear
         end
     end
 end
+
+local function display_init()
+    clear_diplay()
+end
+
 
 local function draw(y, x, n)
     V[0xF] = 0
@@ -105,6 +112,7 @@ local function draw(y, x, n)
             DISPLAY[y % 32][x % 64 + j - 1] = DISPLAY[y % 32][x % 64 + j - 1] ^ MEMORY[I + i + 8 - j]
             V[0xF] = DISPLAY[y % 32][x % 64 + j - 1] & MEMORY[I + i + 8 - j]
         end
+    end
 end
 
 local function load_rom(filename)
@@ -114,46 +122,11 @@ local function load_rom(filename)
 
     assert(#data <= MEMORY_SIZE - PROGRAM_START, "ROM is too large")
     for i = 1, #data do
-        memory[PROGRAM_START + i - 1] = data:byte(i)
+        MEMORY[PROGRAM_START + i - 1] = data:byte(i)
     end
 end
 
-local function fetch()
-    -- to fail in case maybe you fuck up something about pc and go out of bounds
-    assert(
-        pc >= 0 and pc + 1 < MEMORY_SIZE,
-        string.format("PC out of bounds: %04X", pc)
-    )
-
-    local hi = memory[pc]
-    local lo = memory[pc + 1]
-    return (hi << 8) | lo
-end
-
-function love.load(arg)
-    
-    --[[
-    local inp = assert(io.open(arg[1], "rb"))
-    local data = inp:read("*all")
-    assert(inp:close())
- 
-     -- SNIPPET THAT CHECKS INPUT
-    for i = 1, #data, 16 do
-        io.write(string.format("%08x: ", i - 1))
-        for j = 0, 14, 2 do
-            if i + j + 1 <= #data then
-                local high = data:byte(i + j)
-                local low = data:byte(i + j + 1)
-                io.write(string.format("%02x%02x ", high, low))
-            elseif i + j <= #data then
-                io.write(string.format("%02x   ", data:byte(i + j)))
-            end
-        end
-        io.write("\n")
-    end
-    ]] --
-
-    local function execute(opcode)
+local function execute(opcode)
         local op = (opcode >> 12) & 0xF
         local x = (opcode >> 4) & 0xF
         local y = (opcode >> 8) & 0xF
@@ -164,7 +137,7 @@ function love.load(arg)
         if op == 0x0 then
             if x == 0x0 then -- note to self consider NNN == 0x0E0
                 if NN == 0xE0 then
-                    clear_screen();
+                    clear_display();
                 end
             end
         
@@ -209,6 +182,24 @@ function love.load(arg)
         end
     end
 
+local function fetch()
+    -- to fail in case maybe you fuck up something about pc and go out of bounds
+    assert(
+        pc >= 0 and pc + 1 < MEMORY_SIZE,
+        string.format("PC out of bounds: %04X", pc)
+    )
+
+    local hi = memory[pc]
+    local lo = memory[pc + 1]
+    return (hi << 8) | lo
+end
+
+function love.load(arg)
+    
+    load_rom("testibm.ch8")
+    mem_init()
+    display_init()
+
     --[[
     refer to https://en.wikipedia.org/wiki/Endianness#/media/File:32bit-Endianess.svg
     to understand the high-low naming
@@ -224,6 +215,27 @@ function love.load(arg)
 
         pc = pc + 2
     end
+
+    --[[
+    local inp = assert(io.open(arg[1], "rb"))
+    local data = inp:read("*all")
+    assert(inp:close())
+ 
+     -- SNIPPET THAT CHECKS INPUT
+    for i = 1, #data, 16 do
+        io.write(string.format("%08x: ", i - 1))
+        for j = 0, 14, 2 do
+            if i + j + 1 <= #data then
+                local high = data:byte(i + j)
+                local low = data:byte(i + j + 1)
+                io.write(string.format("%02x%02x ", high, low))
+            elseif i + j <= #data then
+                io.write(string.format("%02x   ", data:byte(i + j)))
+            end
+        end
+        io.write("\n")
+    end
+    ]] --
 end
 
 local timer_accumulator = 0
@@ -242,6 +254,7 @@ function love.update(dt)
         end
     else if beep:isPlaying() then
         beep:stop()
+        end
     end
 end
 
@@ -257,7 +270,4 @@ function love.draw()
             love.graphics.rectangle("fill", x * 12, y * 12, 12, 12)
         end
     end
-end
-end
-end
 end
